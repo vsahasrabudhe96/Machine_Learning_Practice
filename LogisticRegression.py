5be782bcb854ba1c188785a17ffc49811f858cb4
Varun Sahasrabudhe <v.sahasrabudhe@yahoo.com>
2021-05-14 01:58:32 -0400

    Final LogosticRedression Commit

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -19,14 +19,14 @@ print("Target/Label Size",digits.target.shape)
 plt.figure(figsize=(20,4))
 for index, (image,target) in enumerate(zip(digits.data[0:5],digits.target[0:5])):
     plt.subplot(1,5,index+1)
-    plt.imshow(np.reshape(image, (8,8),cmap=plt.cm.gray))
+    plt.imshow(np.reshape(image, (8,8)),cmap=plt.cm.gray)
     plt.title('Training: %in' % target, fontsize = 20)
 
 
 ### Split the data 
 from sklearn.model_selection import train_test_split
 
-X_train,X_test,y_train,y_test = train_test_split(digits.data,digits.target,test_size=0.25,random_State=0)
+X_train,X_test,y_train,y_test = train_test_split(digits.data,digits.target,test_size=0.25,random_state=0)
 
 ###Modelling 
 
