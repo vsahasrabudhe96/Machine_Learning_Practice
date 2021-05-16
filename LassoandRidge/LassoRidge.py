e95e8c5387f114bb80c05af3ffe02187e936bf20
Varun Sahasrabudhe <v.sahasrabudhe@yahoo.com>
2021-05-16 00:55:51 -0400

    Lasso Ridge code final, images and readme prefinal

--- LassoandRidge/LassoRidge.py
+++ LassoandRidge/LassoRidge.py
@@ -5,6 +5,79 @@ import seaborn as sns
 
 # LOADING THE DATASET
 
-url = ''
+url = 'https://raw.githubusercontent.com/amankharwal/Website-data/master/Advertising.csv'
 df = pd.read_csv(url)
-df.drop()
+
+df.drop('Unnamed: 0',axis=1,inplace=True)
+
+
+print(df.head())
+
+# Let us plot the graph of dependent to independent variable
+
+def scatter_plot(feature, target):
+    plt.figure(figsize=(16, 18))
+    plt.scatter(df[feature],
+                df[target],
+                c='orange'
+                )
+    plt.xlabel("Money Spent on {} ads ($)".format(feature))
+    plt.ylabel("Sales ($k)")
+    plt.show()
+
+scatter_plot("TV", "Sales")
+scatter_plot("Radio", "Sales")
+scatter_plot("Newspaper", "Sales")
+
+# Let us  run the initial Linear Regression model to check the issues faced in that
+'''
+LINEAR REGRESSION
+'''
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+
+X = df.drop('Sales',axis=1)
+y = df['Sales'].values.reshape(-1,1)
+
+linreg = LinearRegression()
+
+MSE = cross_val_score(linreg,X,y,scoring="neg_mean_squared_error",cv=5)
+
+mean_MSE = np.mean(MSE)
+
+print('Linear Regression mean_MSE: ',mean_MSE)
+
+
+'''
+RIDGE REGRESSION
+'''
+
+from sklearn.model_selection import GridSearchCV
+from sklearn.linear_model import Ridge
+
+ridge = Ridge()
+
+param_dic_ridge = {'alpha':[1e-15, 1e-10, 1e-8, 1e-4, 1e-3, 1e-2, 1, 5, 10, 20]}
+
+ridge_Regression = GridSearchCV(ridge,param_dic_ridge,scoring="neg_mean_squared_error",cv=5)
+
+ridge_Regression.fit(X,y)
+
+print(ridge_Regression.best_params_)
+print("Ridge Regression mean_MSE: ",ridge_Regression.best_score_)
+
+
+'''
+LASSO REGRESSION
+'''
+
+from sklearn.linear_model import Lasso
+lasso = Lasso()
+
+parameters = {"alpha":[1e-15, 1e-10, 1e-8, 1e-4, 1e-3, 1e-2, 1, 5, 10, 20]}
+lasso_regression = GridSearchCV(lasso, parameters, scoring='neg_mean_squared_error', cv=5)
+lasso_regression.fit(X, y)
+
+print(lasso_regression.best_params_)
+print("LASSO Regression mean_MSE: ",lasso_regression.best_score_)
--- LassoandRidge/README.md
+++ LassoandRidge/README.md
@@ -7,4 +7,29 @@
 - # Lasso Regression
     - Least absolute shrinkage and selection operator regression (usually just called lasso regression) is another regularized version of linear regression: just like peak regression, it adds a regularization term to the cost function. , but it uses the ℓ1 norm of the weight vector instead of half the square of the ℓ2 norm.
 
+## Dataset images
 
+- TV ads vs Sales
+![alt text](https://github.com/vsahasrabudhe96/Machine_Learning_Practice/blob/6436fa6968edcdfe9212302884346f7bf1ad54ca/NaiveBayes/GNB_MNB_out.PNG)
+
+- Radio ads vs Sales
+![alt text](https://github.com/vsahasrabudhe96/Machine_Learning_Practice/blob/6436fa6968edcdfe9212302884346f7bf1ad54ca/NaiveBayes/GNB_MNB_out.PNG)
+
+- Newspaper ads vs Sales
+
+![alt text](https://github.com/vsahasrabudhe96/Machine_Learning_Practice/blob/6436fa6968edcdfe9212302884346f7bf1ad54ca/NaiveBayes/GNB_MNB_out.PNG)
+
+
+```
+Linear Regression mean_MSE:  -3.072946597100209
+```
+
+```
+Ridge Regression mean_MSE:  -3.072671338341143
+```
+
+```
+LASSO Regression mean_MSE:  -3.0414058967513684
+```
+
+## As we can see the Lasso Regression performs better than the Linear Model also ensuring that there is no case of overfitting
